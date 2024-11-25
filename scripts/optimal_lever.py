add5b635da317a997f4b490ba431ef5cd83cc230
Niklas von Moers <niklas.von.moers2@t-online.de>
2024-11-25 20:42:22 +0100

    plot theoretically optimal lever instead of simulated one

--- scripts/optimal_lever.py
+++ scripts/optimal_lever.py
@@ -12,24 +12,32 @@ number_paths = 10000
 levers = np.arange(start=0.00, stop=3.5, step=0.01)
 Scenario = namedtuple("Scenario", ["volatility", "expected_return"])
 scenarios = [
-    Scenario(volatility=0.15, expected_return=0.08),
     Scenario(volatility=0.15, expected_return=0.08 - 0.0325),  # S&P 500 minus ECB's interest rate
-    Scenario(volatility=0.20, expected_return=0.08),
+    Scenario(volatility=0.15, expected_return=0.08),
     Scenario(volatility=0.20, expected_return=0.05),
+    Scenario(volatility=0.20, expected_return=0.08),
     Scenario(volatility=0.30, expected_return=0.03),
+    Scenario(volatility=0.30, expected_return=0.08 - 0.0325),
     Scenario(volatility=0.30, expected_return=0.10),
 ]
 
 
 def main() -> None:
+    for scenario in scenarios:
+        theoretically_best_lever = round(
+            np.log(1 + scenario.expected_return) / scenario.volatility**2, 2
+        )
+        levers[-1] = theoretically_best_lever
+    sorted_levers = sorted(levers)
     for scenario in tqdm(scenarios):
         # Maps levers to the final price after n_years.
         final_values: defaultdict[float, list[float]] = defaultdict(list)
+
         for _ in range(number_paths):
             gbm_path = gbm_with_annual_params(
                 n_years * 365, 100.0, scenario.volatility, scenario.expected_return
             )
-            for lever in levers:
+            for lever in sorted_levers:
                 leveraged_path = leverage_asset(gbm_path, lever)
                 final_values[lever].append(leveraged_path[-1])
 
@@ -39,9 +47,11 @@ def main() -> None:
             f"Expected Return: {scenario.expected_return:.2%}"
         )
         plt.plot(medians.keys(), medians.values(), label=label)
-        best_lever = max(medians, key=medians.get)
-        max_median = medians[best_lever]
-        plt.plot(best_lever, max_median, "o", color="black")
+        theoretically_best_lever = round(
+            np.log(1 + scenario.expected_return) / scenario.volatility**2, 2
+        )
+        max_median = medians[theoretically_best_lever]
+        plt.plot(theoretically_best_lever, max_median, "o", color="black")
     plt.title(
         f"Median Final Value After {n_years} Years for Different Levers and Selected Scenarios"
     )
